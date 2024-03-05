7bce428fd2dbd71daeab69b736345e393794386a
1Light <redclover1.618@gmail.com>
2024-03-05 11:11:58 +0300

    upload

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
+if __name__ = "__main__":
 
     names = dir(hidden_4)
     for i in range(len(names)):
-        if names[i][:2] != "__":
-            print(names[i])
+        if not name[i].startswith("__"):
+            print(name[i])
