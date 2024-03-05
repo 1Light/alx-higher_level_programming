a5949ef182c0f16c783d91752d561118ccb990a7
1Light <redclover1.618@gmail.com>
2024-03-05 16:20:07 +0300

    upload

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
-
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    add = __import__("magic_calculation_102").add
-    sub = __import__("magic_calculation_102").sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
