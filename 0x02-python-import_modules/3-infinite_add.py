7fb5840e3b5a3a351fedbf4b02225f4cab7673b2
1Light <redclover1.618@gmail.com>
2024-03-05 09:50:45 +0300

    upload

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
-    num_args = len(argv) - 1
-    count = 0
+    num_args = len(sys.argv) - 1
+    sum = 0
 
-    for i in range(num_args):
-        count += int(argv[i + 1])
-    print("{}".format(count))
+    for _, j in enumerate(sys.argv[1:]):
+
+        sum += int(j)
+    print("{}".format(sum))
