000c9a3fe21ccb0919941292b5ee9b49329766cf
1Light <redclover1.618@gmail.com>
2024-03-05 08:52:48 +0300

    upload

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 if __name__ == "__main__":
     num_args = len(argv) - 1
     if num_args == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
     else:
