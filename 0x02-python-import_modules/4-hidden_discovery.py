721185344e82d674c7ee330450914f1f301eb399
1Light <redclover1.618@gmail.com>
2024-03-05 10:52:14 +0300

    upload

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# 4-hidden_discovery.py
+import hidden_4
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    
+    name = dir(hidden_4)
+    
+    for i in range(len(name)):
+        if not name[i].startswith("__"):
             print(name)
