28636038316d43d0a0fff52ee13eb05bcb7a1b08
1Light <redclover1.618@gmail.com>
2024-03-05 09:44:50 +0300

    upload

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 from sys import argv
 
 if __name__ == "__main__":
-    
+
     num_args = len(argv) - 1
     count = 0
 
     for i in range(num_args):
-        count += int(argv[i+1])
-    print("{}".format(count)) 
+        count += int(argv[i + 1])
+    print("{}".format(count))
