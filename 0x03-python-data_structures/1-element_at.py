568081ee463b9cea969557365799f178ac1186ba
1Light <redclover1.618@gmail.com>
2024-03-06 09:07:00 +0300

    upload

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(idx, my_list=[]):
 
-    if idx < 0 or idx >= len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
         return my_list[idx]
