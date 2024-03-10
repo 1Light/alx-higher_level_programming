039bf863d0862bdf4e06ea31aae8df63732c2a39
1Light <redclover1.618@gmail.com>
2024-03-10 18:12:50 +0300

    upload

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def complex_delete(a_dictionary, value):
 
     my_list = list(a_dictionary.items())
@@ -7,8 +8,8 @@ def complex_delete(a_dictionary, value):
     my_list = [x for x in my_list if x[1] != value]
 
     my_dict = dict(my_list)
-    
+
     a_dictionary.clear()
     a_dictionary.update(my_dict)
-    
+
     return a_dictionary
