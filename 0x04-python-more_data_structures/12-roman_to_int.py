b10fd7fa9f7ae2b4f0bf7b8a81c9694ba8c0501b
1Light <redclover1.618@gmail.com>
2024-03-11 08:45:29 +0300

    upload

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-    }
-
-    nums = [roman_dict[c] for c in roman_string] + [0]
-    result = 0
-
-    for i in range(len(nums) - 1):
-        if nums[i] >= nums[i + 1]:
-            result += nums[i]
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            result -= nums[i]
+            rep -= numbers[i]
 
-    return result
+    return rep
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
+# 2-uniq_add.py
 
 def uniq_add(my_list=[]):
-
-    unique_integers = set()
-
-    for num in my_list:
-        unique_integers.add(num)
-
-    total = sum(unique_integers)
-
-    return total
+    """Add all unique integers in a list (once for each integer)."""
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
