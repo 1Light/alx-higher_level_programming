fed5bb1756066aced37a0d0997a1f97c6d3cbe55
1Light <redclover1.618@.com>
2024-03-13 10:23:17 -0700

    upload

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 -- Lists all privileges of the user user_0d_1.
-=======
--- Lists all privileges of the users user_0d_1 and user_0d_2.
->>>>>>> 0a48f08a453704895a910e3b65ba7342cbdb246e
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- Lists all privileges of the user user_0d_2.
