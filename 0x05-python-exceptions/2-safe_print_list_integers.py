b21b45e3c6f42d95c430c7b465ff1f854824f509
1Light <redclover1.618@gmail.com>
2024-03-15 08:45:50 +0300

    upload

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,7 +17,7 @@ def safe_print_list_integers(my_list=[], x=0):
         print()
         return count
 
-    except(TypeError, ValueError):
+    except (TypeError, ValueError):
         pass
 
     print()
