35c691c86ba8c94d3b48a79b719576b5f364decb
1Light <redclover1.618@gmail.com>
2024-03-15 09:31:16 +0300

    upload

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
+# 3-safe_print_division.py
 
 def safe_print_division(a, b):
-
+    """Returns the quotient of a / b."""
     try:
-        c = a / b
-
-    except ZeroDivisionError:
-        c = None
-
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(c))
-        return c
+        print("Inside result: {}".format(div))
+    return (div)
