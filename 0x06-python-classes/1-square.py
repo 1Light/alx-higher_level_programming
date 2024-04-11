72e77cd7a5676391708e306a0ddff035baf778d2
1Light <redclover1.618@gmail.com>
2024-04-11 09:11:20 +0300

    upload

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+""" This is a script """
+
+
 class Square:
+    """This is a class"""
 
     def __init__(self, size):
+        """This is a method"""
+
         self.__size = size
