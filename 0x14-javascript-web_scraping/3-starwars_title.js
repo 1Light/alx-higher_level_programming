40c1755727fe7724ee5af08c0402e063cf8fabec
1Light <redclover1.618@gmail.com>
2024-05-21 13:55:05 +0300

    upload

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const args = process.argv;
+const requestURL = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+
+request.get(requestURL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
